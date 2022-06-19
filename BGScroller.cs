e084987597cc8f7e4dce103f7a8d3a5d0c28ba68
tan179 <67349521+tan179@users.noreply.github.com>
2022-06-19 22:26:25 +0800

    Add files via upload
    游戏脚本

---
+++ BGScroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+    public float scrollSpeed;
+    private Vector3 startPos;
+    // Use this for initialization
+    void Start() {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float dis = Mathf.Repeat(scrollSpeed * Time.time, 30);
+        transform.position = startPos + dis * Vector3.forward * (-1);
+    }
+}
---
+++ ContactCheck.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactCheck : MonoBehaviour
+{
+    //敌人自身爆炸特效
+    public GameObject enemyExplosion;
+    //主角飞船的爆炸特效
+    public GameObject playerExplosion;
+
+    //物体被销毁时玩家获得的分数
+    public int scoreValue;
+
+    private GameMgr gameMgr;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("GameMgr");
+        gameMgr = go.GetComponent<GameMgr>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+        //播放敌人自身爆炸特效
+        if (enemyExplosion != null)
+        {
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
+            if (other.tag != "Weapon")
+            {
+                Destroy(other.gameObject);
+            }
+            
+        }
+
+        //增加UI分数
+        gameMgr.AddScore(scoreValue);
+
+        //播放主角飞船的爆炸特效
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject.transform.parent.gameObject);
+            gameMgr.GameOver();
+        }
+
+
+
+        Destroy(gameObject);
+    }
+}
